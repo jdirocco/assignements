4dda5bfb35c735cd364f4bd3d8850c2b4b334c25
jdirocco <juri.di.rocco@gmail.com>
2022-06-13 15:17:21 +0200

    assignement2: Remove commented-out code

--- assignement2/src/main/java/com/nexthink/assigments/assignement2/ExprVisitorImpl.java
+++ assignement2/src/main/java/com/nexthink/assigments/assignement2/ExprVisitorImpl.java
@@ -32,26 +32,6 @@ public class ExprVisitorImpl extends ExprBaseVisitor<Integer> {
 			return visitExpr((ExprContext) ctx.getChild(1));
 
 		return 0;
-//    	if (ctx.getChild(0).getText().startsWith("("))
-//    		return visit(ctx.getChild(0).getChild(1));
-//        if (ctx.getChild(1) == null) {
-//            throw new UnsupportedOperationException("An operator of +, -, /, * is required to perform the operation");
-//        }
-//        String operator = ctx.getChild(1).getText();
-//        int left = Integer.parseInt(ctx.getChild(0).getText());
-//        int right = Integer.parseInt(ctx.getChild(2).getText());
-//
-//        switch (operator) {
-//            case "+":
-//                return left + right;
-//            case "-":
-//                return left - right;
-//            case "/":
-//                return left / right;
-//            case "*":
-//                return left * right;
-//            default:
-//                throw new UnsupportedOperationException("Calculator does not support " + operator);
-//        }
+
 	}
 }
